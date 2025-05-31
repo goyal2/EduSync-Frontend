f6302068f27f062f58254938bb8446566ad9c2de
SomyaGoyal <115345500+goyal2@users.noreply.github.com>
2025-05-31 09:31:44 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'backendprojectwebapp-c4azccb4dbbchsdc.centralindia-01.azurewebsites.net',
+  baseURL: 'https://backendprojectwebapp-c4azccb4dbbchsdc.centralindia-01.azurewebsites.net',
   headers: {
     'Content-Type': 'application/json',
   },
